7dba8e28221101d95ccf5d6f745104732da05a2d
Andy <onsubmit@outlook.com>
2022-08-28 19:34:49 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Andy Young">
+//     Copyright (c) Andy Young. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Diagnostics;
+
+string input = "watch.txt";
+if (args.Length > 0 && File.Exists(args[0]))
+{
+    input = args[0];
+}
+
+if (!File.Exists(input))
+{
+    throw new FileNotFoundException(input);
+}
+
+foreach (string line in await File.ReadAllLinesAsync(input))
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        // Empty line
+        continue;
+    }
+
+    if (line.StartsWith('#'))
+    {
+        // Comment
+        continue;
+    }
+
+    string expanded = Environment.ExpandEnvironmentVariables(line);
+    Console.WriteLine(expanded);
+    if (!File.Exists(expanded))
+    {
+        Console.WriteLine("File doesn't exist.");
+        continue;
+    }
+
+    string filename = Path.GetFileNameWithoutExtension(expanded);
+    Process[] processes = Process.GetProcessesByName(filename);
+    if (processes.Any())
+    {
+        Console.WriteLine("Process already running.");
+        continue;
+    }
+
+    Console.WriteLine("Starting process.");
+    Process.Start(expanded);
+}
